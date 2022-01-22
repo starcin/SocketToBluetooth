7a708a064d0cc61fbbdc43dd5ef48705f1222225
starcin <starcin@gmail.com>
2022-01-22 15:59:23 +0300

    BucketButton componenti oluşturulup, butonlar buradan çağırıldı

--- client/src/components/BucketControl/BucketControl.js
+++ client/src/components/BucketControl/BucketControl.js
@@ -1,7 +1,4 @@
-import bucket_up from "./bucket_up.svg"
-import bucket_down from "./bucket_down.svg"
-import { ReactComponent as BucketUp } from "./bucket_up.svg"
-import { ReactComponent as BucketDown } from "./bucket_down.svg"
+import BucketButton from "./BucketButton"
 
 function BucketControl({ bucketCommandEvent }) {
 	function bucketUp() {
@@ -18,28 +15,16 @@ function BucketControl({ bucketCommandEvent }) {
 
 	return (
 		<div className="flex flex-col w-24 h-40">
-			<button
-				className="bg-[url('components/BucketControl/button_bg.svg')] group p-4 h-1/2 flex items-center rounded-t-2xl"
-				onMouseDown={bucketUp}
-				onMouseUp={bucketStop}
-				onMouseLeave={bucketStop}
-			>
-				{/* <img
-					className="fill-orange-400 stroke-violet-500 text-green-400"
-					src={bucket_up}
-					alt="bucket_up"
-				/> */}
-				<BucketUp className="fill-gray-300 group-active:stroke-amber-300 group-active:stroke-[12px] group-active:fill-amber-100" />
-			</button>
-			<button
-				className="bg-[url('components/BucketControl/button_bg.svg')] group p-4 h-1/2 flex items-center rounded-b-2xl"
-				onMouseDown={bucketDown}
-				onMouseUp={bucketStop}
-				onMouseLeave={bucketStop}
-			>
-				{/* <img className="text-stone-900" src={bucket_down} alt="bucket_down" /> */}
-				<BucketDown className="fill-gray-300 group-active:stroke-amber-300 group-active:stroke-[12px] group-active:fill-amber-100" />
-			</button>
+			<BucketButton
+				direction={"UP"}
+				onButtonUp={bucketUp}
+				onButtonDown={bucketStop}
+			/>
+			<BucketButton
+				direction={"DOWN"}
+				onButtonUp={bucketDown}
+				onButtonDown={bucketStop}
+			/>
 		</div>
 	)
 }
